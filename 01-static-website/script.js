88af3f870fcbb7dd2e8734de5ace90691446e450
Ram Murat Sharma <rammurat.rms.sharma0@gmail.com>
2020-05-17 12:42:00 +0100

    Static html files created for AWS deployment

---
+++ 01-static-website/script.js
@@ -0,0 +1,13 @@
+/*
+
+https://ionlyseespots.github.io/ambient-design/index.html
+
+I am building a new HTML5 + flexbox methodology to build UI. Any thoughts or insight is appreciated!
+
+Ambient is a lightweight and modular HTML5 UI development methodology. Ambient make use of HTML5 sections & outlines for semantic scaffolding and CSS3 attributes on elements for page layout.
+
+Ambient is ready to adopt whatever your design or style is. Feel free to use HTML, whenever and wherever you like. Build the intent of your content. You can add additional layout values as well as extend child attributes to your scaffolding to gradually build a hierarchy that defines your layout.
+
+*/
+
+console.log('Main script loaded...')
